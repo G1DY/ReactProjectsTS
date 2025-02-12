0cdd758067a1ba87e81b7fbf2102014db586530e
gideon <kipkerring.gedion@gmail.com>
2025-02-12 12:14:22 +0300

    creating a EventHandler using typescript

--- reactTypescript/src/Components/EventHandling.tsx
+++ reactTypescript/src/Components/EventHandling.tsx
@@ -1,4 +1,15 @@
 const EventHandling = () => {
-  return <div>EventHandling</div>;
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("Button Clicked", e.currentTarget);
+  };
+  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+    console.log("Mouse Entered!", e.currentTarget);
+  };
+  return (
+    <div onMouseEnter={handleMouseEnter}>
+      <h2>Handling Events Examples</h2>
+      <button onClick={handleClick}>Clicked</button>
+    </div>
+  );
 };
 export default EventHandling;
